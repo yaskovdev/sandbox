7171b406e2313e2f484dc9763ae6778e2f8b00d5
Sergey Yaskov <yaskovdev@gmail.com>
2017-08-19 19:54:27 +0300

    Make the JMS client configurable.

--- distributed-transaction-sandbox/src/main/java/com/yaskovdev/sandbox/distributedtransaction/client/JmsClient.java
+++ distributed-transaction-sandbox/src/main/java/com/yaskovdev/sandbox/distributedtransaction/client/JmsClient.java
@@ -4,10 +4,10 @@ import com.yaskovdev.sandbox.distributedtransaction.exception.CannotSendNotifica
 import com.yaskovdev.sandbox.distributedtransaction.model.Notification;
 import org.apache.activemq.ActiveMQConnectionFactory;
 import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.jms.Connection;
-import javax.jms.DeliveryMode;
 import javax.jms.Destination;
 import javax.jms.JMSException;
 import javax.jms.MessageProducer;
@@ -15,6 +15,8 @@ import javax.jms.Session;
 import javax.jms.TextMessage;
 
 import static java.lang.Thread.currentThread;
+import static javax.jms.DeliveryMode.NON_PERSISTENT;
+import static javax.jms.Session.AUTO_ACKNOWLEDGE;
 import static org.slf4j.LoggerFactory.getLogger;
 
 @Component
@@ -22,19 +24,29 @@ public class JmsClient {
 
     private static final Logger logger = getLogger(JmsClient.class);
 
+    private final String brokerUrl;
+
+    private final String queueName;
+
+    public JmsClient(@Value("${jms.broker.url}") final String brokerUrl,
+            @Value("${jms.queue.name}") final String queueName) {
+        this.brokerUrl = brokerUrl;
+        this.queueName = queueName;
+    }
+
     public void sendNotification(final Notification notification) {
         try {
-            final ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory("tcp://127.0.0.1:61616");
+            final ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory(brokerUrl);
 
             final Connection connection = connectionFactory.createConnection();
             connection.start();
 
-            final Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+            final Session session = connection.createSession(false, AUTO_ACKNOWLEDGE);
 
-            final Destination destination = session.createQueue("TEST.FOO");
+            final Destination destination = session.createQueue(queueName);
 
             final MessageProducer producer = session.createProducer(destination);
-            producer.setDeliveryMode(DeliveryMode.NON_PERSISTENT);
+            producer.setDeliveryMode(NON_PERSISTENT);
 
             final String text = "Got " + notification + " from: " + currentThread().getName() + " : " + hashCode();
             final TextMessage message = session.createTextMessage(text);
