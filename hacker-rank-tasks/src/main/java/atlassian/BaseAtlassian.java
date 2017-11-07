bbb58a5be7b710655f00d77dbade578f9193918a
Sergey Yaskov <yaskovdev@gmail.com>
2017-11-07 18:53:30 +0200

    Solve base 7 problem.

--- hacker-rank-tasks/src/main/java/atlassian/BaseAtlassian.java
+++ hacker-rank-tasks/src/main/java/atlassian/BaseAtlassian.java
@@ -7,36 +7,37 @@ public class BaseAtlassian {
 
     private static final int BASE = 7;
 
-    private static Map<Integer, Character> map = map();
+    private static Map<Long, Character> map = map();
 
     public static void main(String[] args) {
         System.out.println(base(7792875));
+        System.out.println(base(1000000000000L));
     }
 
-    private static String base(final int number) {
-        int n = number;
+    private static String base(final long number) {
+        long n = number;
         final StringBuilder result = new StringBuilder();
         while (n > 0) {
-            int value = n % BASE;
+            long value = n % BASE;
             result.append(letterFor(value));
             n = (n - value) / BASE;
         }
         return result.reverse().toString();
     }
 
-    private static char letterFor(int value) {
+    private static char letterFor(long value) {
         return map.get(value);
     }
 
-    private static Map<Integer, Character> map() {
-        final HashMap<Integer, Character> map = new HashMap<>();
-        map.put(0, '0');
-        map.put(1, 'a');
-        map.put(2, 't');
-        map.put(3, 'l');
-        map.put(4, 's');
-        map.put(5, 'i');
-        map.put(6, 'n');
+    private static Map<Long, Character> map() {
+        final HashMap<Long, Character> map = new HashMap<>();
+        map.put(0L, '0');
+        map.put(1L, 'a');
+        map.put(2L, 't');
+        map.put(3L, 'l');
+        map.put(4L, 's');
+        map.put(5L, 'i');
+        map.put(6L, 'n');
         return map;
     }
 }
