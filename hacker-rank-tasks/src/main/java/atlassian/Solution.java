97a6b7b91602a5d90062a71b6cb79bc151650714
Sergey Yaskov <yaskovdev@gmail.com>
2017-11-08 18:55:30 +0200

    Solve the task about the lists.

--- hacker-rank-tasks/src/main/java/atlassian/Solution.java
+++ hacker-rank-tasks/src/main/java/atlassian/Solution.java
@@ -1,22 +1,20 @@
 package atlassian;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.Arrays;
 
 public class Solution {
 
-    public static class LinkedListNode {
+    private static class LinkedListNode {
         String val;
         LinkedListNode next;
 
-        LinkedListNode(String node_value) {
-            val = node_value;
+        private LinkedListNode(String nodeValue) {
+            val = nodeValue;
             next = null;
         }
     }
 
-    public static LinkedListNode insert(LinkedListNode head, String val) {
+    private static LinkedListNode insert(LinkedListNode head, String val) {
         if (head == null) {
             head = new LinkedListNode(val);
         } else {
@@ -24,28 +22,50 @@ public class Solution {
             while (end.next != null) {
                 end = end.next;
             }
-            LinkedListNode node = new LinkedListNode(val);
-            end.next = node;
+            end.next = new LinkedListNode(val);
+        }
+        return head;
+    }
+
+    private static LinkedListNode list(final String... values) {
+        final LinkedListNode list = insert(null, values[0]);
+        LinkedListNode head = list;
+        for (final String value : Arrays.copyOfRange(values, 1, values.length)) {
+            head = insert(list, value);
         }
         return head;
     }
 
     public static void main(final String[] args) {
-
+        System.out.println(find(list("1", "2", "3"), list("2", "3")));
+        System.out.println(find(list("1", "2", "3"), list("3", "2")));
+        System.out.println(find(list("1", "2", "3"), list("1", "2")));
+        System.out.println(find(list("1", "3", "2", "1", "2"), list("3", "2", "1")));
     }
 
-    static int find(LinkedListNode list, LinkedListNode subListStart) {
-        final List<String> a = new ArrayList<>();
-        final List<String> b = new ArrayList<>();
+    static void print(final LinkedListNode list) {
+        LinkedListNode node = list;
+        while (node != null) {
+            System.out.print(node.val + " ");
+            node = node.next;
+        }
+    }
 
-        while (list != null) {
-            a.add(list.val);
-            list = list.next;
+    private static int find(LinkedListNode list, LinkedListNode subListStart) {
+        LinkedListNode i = list;
+        LinkedListNode j = subListStart;
+        int count = 0;
+        while (i != null && j != null) {
+            if (i.val.equals(j.val)) {
+                i = i.next;
+                j = j.next;
+            } else if (j == subListStart) {
+                i = i.next;
+                count++;
+            } else {
+                j = subListStart;
+            }
         }
-        while (subListStart != null) {
-            b.add(subListStart.val);
-            subListStart = subListStart.next;
-        }
-        return Collections.indexOfSubList(a, b);
+        return j == null ? count : -1;
     }
 }
