bd2a792f1b3d5156d6cc999341fb9add2080d3f1
Sergey Yaskov <yaskovdev@gmail.com>
2017-11-14 20:20:02 +0200

    Cleanup code.

--- hacker-rank-tasks/src/main/java/atlassian/RobotState.java
+++ hacker-rank-tasks/src/main/java/atlassian/RobotState.java
@@ -2,43 +2,36 @@ package atlassian;
 
 import static java.util.Arrays.fill;
 
-/**
- * Created by Sergey on 14.11.2017.
- */
-public class RobotState {
+class RobotState {
 
     private final int[] pile;
     private int position = -1;
     private boolean holdsBlock = false;
 
-    public RobotState() {
+    RobotState() {
         pile = new int[10];
         fill(pile, 0);
     }
 
-    public void pickup() {
-        if (!holdsBlock) {
-            holdsBlock = true;
-        }
+    void pickup() {
+        holdsBlock = true;
         position = 0;
     }
 
-    public void move() {
+    void move() {
         if (position < 9) {
             position++;
         }
     }
 
-    public void lower() {
-        if (holdsBlock) {
-            if (pile[position] < 15) {
-                pile[position]++;
-                holdsBlock = false;
-            }
+    void lower() {
+        if (holdsBlock && pile[position] < 15) {
+            pile[position]++;
+            holdsBlock = false;
         }
     }
 
-    public int[] getPile() {
+    int[] getPile() {
         return pile;
     }
 }
