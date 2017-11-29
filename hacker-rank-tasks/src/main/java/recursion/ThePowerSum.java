b20dc8545e8bea8a6a00aa1105c2125e8631b152
Sergey Yaskov <yaskovdev@gmail.com>
2017-11-29 20:11:46 +0200

    Another attempt.

--- hacker-rank-tasks/src/main/java/recursion/ThePowerSum.java
+++ hacker-rank-tasks/src/main/java/recursion/ThePowerSum.java
@@ -2,6 +2,7 @@ package recursion;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 
 public class ThePowerSum {
@@ -10,23 +11,29 @@ public class ThePowerSum {
 //        final Scanner scanner = new Scanner(System.in);
 //        final int number = scanner.nextInt();
 //        final int power = scanner.nextInt();
-        System.out.println(solve(100, Collections.<Integer>emptyList()));
+        System.out.println(ways(100, Collections.<Integer>emptyList()));
     }
 
     private static int solve(final int number, final int power) {
         return sqrt(number);
     }
 
+    private static int sq(final int number) {
+        return (int) Math.pow(number, 2);
+    }
+
     private static int sqrt(final int number) {
         return (int) Math.pow(number, 1.0 / 2);
     }
 
-    private static int solve(final int number, final List<Integer> elements) {
-        if (number == 0 || number == 1) {
+    private static int ways(final int number, final List<Integer> elements) {
+        if (hasDuplicates(elements)) {
+            return 0;
+        } else if (number == 0) {
             return 1;
         } else {
             final int m = sqrt(number);
-            return elements.contains(m) ? 0 : solve(number - (int) Math.pow(m, 2), concat(elements, m));
+            return ways(number - sq(m), concat(elements, m)) + ways(number - sq(m - 1), concat(elements, m - 1));
         }
     }
 
@@ -34,5 +41,9 @@ public class ThePowerSum {
         final List<Integer> result = new ArrayList<>(elements);
         result.add(m);
         return result;
+    }
+
+    private static boolean hasDuplicates(final List<Integer> list) {
+        return new HashSet<>(list).size() < list.size();
     }
 }
