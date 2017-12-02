f268060a702fa49b5c24af79741dae866d4e8c6b
Sergey Yaskov <yaskovdev@gmail.com>
2017-12-02 10:46:00 +0200

    Refactor the solution.

--- printing-2d-matrix/src/Main.java
+++ printing-2d-matrix/src/Main.java
@@ -1,11 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
-        int[][] matrix = create(1, 8);
+    public static void main(final String[] args) {
+        final Scanner scanner = new Scanner(System.in);
+        final int n = scanner.nextInt();
+        final int[][] matrix = create(1, (int) Math.pow(2, n));
         print(matrix);
     }
 
-    private static int[][] create(int startValue, int size) {
+    private static int[][] create(final int startValue, final int size) {
         if (size == 1) {
             return new int[][]{{startValue}};
         } else {
@@ -16,37 +20,25 @@ public class Main {
         }
     }
 
-    private static int[][] combine(int[][] m1, int[][] m2, int[][] m3, int[][] m4) {
-        int initialSize = m1.length;
-        int sizeOfResult = initialSize * 2;
-        int[][] result = new int[sizeOfResult][sizeOfResult];
+    private static int[][] combine(final int[][] m1, final int[][] m2, final int[][] m3, final int[][] m4) {
+        final int initialSize = m1.length;
+        final int sizeOfResult = initialSize * 2;
+        final int[][] result = new int[sizeOfResult][sizeOfResult];
         for (int row = 0; row < initialSize; row++) {
             for (int col = 0; col < initialSize; col++) {
                 result[row][col] = m1[row][col];
-            }
-        }
-        for (int row = 0; row < initialSize; row++) {
-            for (int col = 0; col < initialSize; col++) {
                 result[row][col + initialSize] = m2[row][col];
-            }
-        }
-        for (int row = 0; row < initialSize; row++) {
-            for (int col = 0; col < initialSize; col++) {
                 result[row + initialSize][col] = m3[row][col];
-            }
-        }
-        for (int row = 0; row < initialSize; row++) {
-            for (int col = 0; col < initialSize; col++) {
                 result[row + initialSize][col + initialSize] = m4[row][col];
             }
         }
         return result;
     }
 
-    private static void print(int[][] matrix) {
-        for (int[] row : matrix) {
-            for (int val : row) {
-                System.out.printf("%3d", val);
+    private static void print(final int[][] matrix) {
+        for (final int[] row : matrix) {
+            for (final int val : row) {
+                System.out.printf("%-3d", val);
             }
             System.out.println();
         }
