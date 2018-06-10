759339342c0f21d3e70b68749042b1cd57920686
yaskovdev <yaskovdev@gmail.com>
2018-06-10 21:41:36 +0300

    Update the sandbox before asking the question.

--- spring-data-sandbox/curl/create.sh
+++ spring-data-sandbox/curl/create.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
 
 curl -v -H "Content-Type: application/json" --data @./role.json http://localhost:8080/roles
-
-curl -v -H "Content-Type: application/json" --data @./updated-role.json http://localhost:8080/roles/CODE
--- spring-data-sandbox/src/main/resources/database.sql
+++ spring-data-sandbox/src/main/resources/database.sql
@@ -1,19 +1,3 @@
-CREATE TABLE role (
-  id   INTEGER PRIMARY KEY,
-  code VARCHAR(40),
-  name VARCHAR(40)
-);
+CREATE DATABASE spring_data_sandbox;
 
-CREATE TABLE privilege (
-  id      INTEGER PRIMARY KEY,
-  role_id INTEGER REFERENCES role,
-  code    VARCHAR(40),
-  name    VARCHAR(40)
-);
-
-DELETE FROM privilege;
-DELETE FROM role;
-
-INSERT INTO role (id, name) VALUES (2000, 'ROLE_ADMIN');
-INSERT INTO privilege (id, role_id, name) VALUES (5000, 2000, 'PRIVILEGE_CREATE_USER');
-INSERT INTO privilege (id, role_id, name) VALUES (5001, 2000, 'PRIVILEGE_DELETE_USER');
+ALTER TABLE PRIVILEGE ADD CONSTRAINT U__ROLE_ID__CODE__PRIVILEGE UNIQUE (ROLE_ID, CODE);
