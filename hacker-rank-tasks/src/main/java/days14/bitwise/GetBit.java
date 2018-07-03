147de77a1452bebfbf6959ecb16c320458ec156e
yaskovdev <yaskovdev@gmail.com>
2018-07-03 19:45:51 +0300

    Start implementing bitwise operations.

---
+++ hacker-rank-tasks/src/main/java/days14/bitwise/GetBit.java
@@ -0,0 +1,32 @@
+package days14.bitwise;
+
+import days14.Node;
+
+import java.util.Collections;
+import java.util.List;
+
+public class GetBit {
+
+    public static void main(String[] args) {
+        final List<Node> nodes = Collections.emptyList();
+        for (Node node : nodes) {
+            process(node);
+        }
+        nodes.forEach(GetBit::process);
+        Integer number = 13;
+        final String bits = Integer.toBinaryString(number);
+        System.out.println(bits);
+        System.out.println(bit(number, 0));
+        System.out.println(bit(number, 1));
+        System.out.println(bit(number, 2));
+        System.out.println(bit(number, 3));
+    }
+
+    private static void process(Node node) {
+
+    }
+
+    private static boolean bit(final int number, final int index) {
+        return ((number >>> index) & 1) == 1;
+    }
+}
