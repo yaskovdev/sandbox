e388928e6b65ecd4f897bc6d313839a208bdae8b
yaskovdev <yaskovdev@gmail.com>
2018-09-11 22:08:52 +0300

    Create new project for interview preparation

--- cracking-the-coding-interview/src/main/java/chapter5/Task1.java
+++ cracking-the-coding-interview/src/main/java/chapter5/Task1.java
@@ -1,4 +1,10 @@
 package chapter5;
 
-public class Task1 {
+// TODO: check the solution
+class Task1 {
+
+    int solve(final int n, final int m, final int i, final int j) {
+        final int mask = (-1 << (j + 1)) + (1 << i) - 1;
+        return (n & mask) + (m << i);
+    }
 }
