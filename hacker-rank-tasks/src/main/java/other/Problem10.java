a04c37082a6e6b9c655a4f1f2eee288a0469b2be
yaskovdev <yaskovdev@gmail.com>
2018-09-16 16:15:29 +0300

    More exercises.

--- hacker-rank-tasks/src/main/java/other/Problem10.java
+++ hacker-rank-tasks/src/main/java/other/Problem10.java
@@ -3,53 +3,55 @@ package other;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Problem10 {
+class Problem10 {
 
-    public boolean isMatch(String s, String p) {
+    boolean isMatch(String s, String p) {
         System.out.println(toGroups(p));
-        return false;
+        return process(s, toGroups(p));
+    }
+
+    private boolean process(final String input, List<Group> groups) {
+        if (groups.isEmpty() && input.isEmpty()) {
+            return true;
+        } else if (!groups.isEmpty() && input.isEmpty()) {
+            return false;
+        } else if (groups.isEmpty() && !input.isEmpty()) {
+            return false;
+        } else {
+            final Group g = groups.get(0);
+            int i = 0;
+            while (i <= input.length() && g.isMatchedBy(input.substring(0, i))) {
+                i++;
+            }
+            if (g.isFullyMatchedBy(input.substring(0, i - 1))) {
+                return process(input.substring(i - 1), groups.subList(1, groups.size()));
+            } else {
+                return false;
+            }
+        }
     }
 
     private List<Group> toGroups(final String pattern) {
         final List<Group> groups = new ArrayList<>();
-        String soFar = "";
+        final StringBuilder buffer = new StringBuilder();
         for (int i = 0; i < pattern.length(); i++) {
             final char c = pattern.charAt(i);
-            if (Character.isLetter(c)) {
-                Character afterNext = afterNext(pattern, i);
-                if (afterNext == null) {
-                    soFar += c;
-                } else if (afterNext == '*') {
-                    groups.add(new Group(soFar));
-                    soFar = c + "";
-                } else {
-                    soFar += c;
-                }
+            if (Character.isLetter(c) || c == '.') {
+                buffer.append(c);
             } else if (c == '*') {
-                soFar += c;
-                groups.add(new Group(soFar));
-                soFar = "";
-            } else if (c == '.') {
-                Character next = next(pattern, i);
-                if (next != null && next == '*') {
-                    soFar += c;
-                } else {
-                    soFar += c;
-                    groups.add(new Group(soFar));
-                    soFar = "";
+                final String word = buffer.substring(0, buffer.length() - 1);
+                if (!word.isEmpty()) {
+                    groups.add(new Group(word));
                 }
+                groups.add(new Group(buffer.charAt(buffer.length() - 1) + "*"));
+                buffer.setLength(0);
             } else {
                 throw new RuntimeException("unexpected character " + c);
             }
         }
+        if (buffer.length() > 0) {
+            groups.add(new Group(buffer.toString()));
+        }
         return groups;
-    }
-
-    private static Character next(String pattern, int i) {
-        return i + 1 < pattern.length() ? pattern.charAt(i + 1) : null;
-    }
-
-    private Character afterNext(String pattern, int i) {
-        return i + 2 < pattern.length() ? pattern.charAt(i + 2) : null;
     }
 }
--- hacker-rank-tasks/src/test/java/other/Problem10Test.java
+++ hacker-rank-tasks/src/test/java/other/Problem10Test.java
@@ -2,11 +2,43 @@ package other;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 public class Problem10Test {
 
     @Test
     public void test1() {
-        new Problem10().isMatch("", "mis*is*p*.");
+        assertFalse(new Problem10().isMatch("mississippi", "mis*is*p*."));
     }
 
+    @Test
+    public void test2() {
+        assertTrue(new Problem10().isMatch("mississippi", "m.s*is*ip*i"));
+    }
+
+    @Test
+    public void test4() {
+        assertFalse(new Problem10().isMatch("aa", "a"));
+    }
+
+    @Test
+    public void test5() {
+        assertTrue(new Problem10().isMatch("aa", "a*"));
+    }
+
+    @Test
+    public void test6() {
+        assertTrue(new Problem10().isMatch("ab", ".*"));
+    }
+
+    @Test
+    public void test3() {
+        assertTrue(new Problem10().isMatch("aab", "c*a*b"));
+    }
+
+    @Test
+    public void test7() {
+        assertTrue(new Problem10().isMatch("aaa", "a*a"));
+    }
 }
