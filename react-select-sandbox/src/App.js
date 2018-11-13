c566d89cd8b930ec63c9a42d8a9a1fd2032f82a4
Sergei Iaskov <sergei.iaskov@omniva.ee>
2018-11-13 14:00:55 +0200

    Commit latest changes.

--- react-select-sandbox/src/App.js
+++ react-select-sandbox/src/App.js
@@ -1,21 +1,48 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Select from 'react-select';
+import 'react-select/dist/react-select.css'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+
+	state = { value: '' };
+
+	handleChange = (value) => this.setState({ value });
+
+	render() {
+		const options =
+			[
+				{
+					value: 'foo', label: 'Foo'
+				},
+				{
+					value: 'bar', label: 'Bar'
+				},
+				{
+					value: 'baz', label: 'Baz'
+				}
+			];
+
+		return (
+			<div className="App" style={{ maxWidth: '20%' }}>
+				<header className="App-header">
+					<img src={logo} className="App-logo" alt="logo" />
+					<h1 className="App-title">Welcome to React</h1>
+				</header>
+				<p className="App-intro">
+					To get started, edit <code>src/App.js</code> and save to reload.
+				</p>
+				<Select
+					value={this.state.value}
+					name="filter__statistics"
+					options={options}
+					onChange={this.handleChange}
+					multi={true}
+				/>
+			</div>
+		);
+	}
 }
 
 export default App;
