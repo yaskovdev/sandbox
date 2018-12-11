e8185c128f784149a5923e7d09e6e05a0d180515
yaskovdev <yaskovdev@gmail.com>
2018-12-11 23:50:38 +0200

    Create a sample project that reproduces the issue with the TextInput component of React Native.

--- react-native-text-input-problem/App.js
+++ react-native-text-input-problem/App.js
@@ -8,7 +8,7 @@ export default class App extends React.Component {
     render() {
         return (
             <View style={styles.container}>
-                <TextInput style={{borderColor: '#000'}} value={this.state.value} onChangeText={value => this.setState({value})}/>
+                <TextInput value={this.state.value} onChangeText={value => this.setState({value})}/>
             </View>
         );
     }
