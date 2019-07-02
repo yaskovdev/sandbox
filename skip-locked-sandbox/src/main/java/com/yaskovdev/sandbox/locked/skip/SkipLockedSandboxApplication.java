c22dc226e7997b5ca95342c159b187f69e8d9475
yaskovdev <yaskovdev@gmail.com>
2019-07-02 23:07:46 +0300

    Switch to the newer Spring Boot version in order to use the SQL dialect that supports skip locked.

--- skip-locked-sandbox/src/main/java/com/yaskovdev/sandbox/locked/skip/SkipLockedSandboxApplication.java
+++ skip-locked-sandbox/src/main/java/com/yaskovdev/sandbox/locked/skip/SkipLockedSandboxApplication.java
@@ -2,18 +2,11 @@ package com.yaskovdev.sandbox.locked.skip;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SkipLockedSandboxApplication extends SpringBootServletInitializer {
+public class SkipLockedSandboxApplication {
 
-	@Override
-	protected SpringApplicationBuilder configure(final SpringApplicationBuilder application) {
-		return application.sources(SkipLockedSandboxApplication.class);
-	}
-
-	public static void main(final String[] args) {
-		SpringApplication.run(SkipLockedSandboxApplication.class, args);
-	}
+    public static void main(final String[] args) {
+        SpringApplication.run(SkipLockedSandboxApplication.class, args);
+    }
 }
