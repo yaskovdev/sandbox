52e8a6eab5bb54de5985df9bb2521c45288bbdd7
yaskovdev <yaskovdev@gmail.com>
2021-11-20 20:15:29 +0200

    Create new ffmpeg sandbox.

---
+++ ffmpeg-encoder/main.cpp
@@ -0,0 +1,143 @@
+#include<cstdio>
+#include<cstdlib>
+
+extern "C" {
+#include <libavcodec/avcodec.h>
+#include "libavutil/frame.h"
+#include "libavutil/imgutils.h"
+}
+
+static void encode(AVCodecContext *enc_ctx, AVFrame *frame, AVPacket *pkt, FILE *outfile) {
+    int ret;
+
+    /* send the frame to the encoder */
+    ret = avcodec_send_frame(enc_ctx, frame);
+    if (ret < 0) {
+        fprintf(stderr, "error sending a frame for encoding\n");
+        exit(1);
+    }
+
+    while (ret >= 0) {
+        ret = avcodec_receive_packet(enc_ctx, pkt);
+        if (ret == AVERROR(EAGAIN) || ret == AVERROR_EOF)
+            return;
+        else if (ret < 0) {
+            fprintf(stderr, "error during encoding\n");
+            exit(1);
+        }
+
+        printf("encoded frame %3" PRId64 " (size=%5d)\n", pkt->pts, pkt->size);
+        fwrite(pkt->data, 1, pkt->size, outfile);
+        av_packet_unref(pkt);
+    }
+}
+
+int main(int argc, char **argv) {
+    const char *filename;
+    const AVCodec *codec;
+    AVCodecContext *c = nullptr;
+    int i, ret, x, y;
+    FILE *f;
+    AVFrame *picture;
+    AVPacket *pkt;
+    uint8_t end_code[] = {0, 0, 1, 0xb7};
+
+    if (argc <= 1) {
+        fprintf(stderr, "Usage: %s <output file>\n", argv[0]);
+        exit(0);
+    }
+    filename = argv[1];
+
+    /* find the mpeg1video encoder */
+    codec = avcodec_find_encoder(AV_CODEC_ID_MPEG1VIDEO);
+    if (!codec) {
+        fprintf(stderr, "codec not found\n");
+        exit(1);
+    }
+
+    c = avcodec_alloc_context3(codec);
+    picture = av_frame_alloc();
+
+    pkt = av_packet_alloc();
+    if (!pkt)
+        exit(1);
+
+    /* put sample parameters */
+    c->bit_rate = 400000;
+    /* resolution must be a multiple of two */
+    c->width = 352;
+    c->height = 288;
+    /* frames per second */
+    c->time_base = (AVRational) {1, 25};
+    c->framerate = (AVRational) {25, 1};
+
+    c->gop_size = 10; /* emit one intra frame every ten frames */
+    c->max_b_frames = 1;
+    c->pix_fmt = AV_PIX_FMT_YUV420P;
+
+    /* open it */
+    if (avcodec_open2(c, codec, nullptr) < 0) {
+        fprintf(stderr, "could not open codec\n");
+        exit(1);
+    }
+
+    f = fopen(filename, "wb");
+    if (!f) {
+        fprintf(stderr, "could not open %s\n", filename);
+        exit(1);
+    }
+
+    picture->format = c->pix_fmt;
+    picture->width = c->width;
+    picture->height = c->height;
+
+    ret = av_frame_get_buffer(picture, 32);
+    if (ret < 0) {
+        fprintf(stderr, "could not alloc the frame data\n");
+        exit(1);
+    }
+
+    /* encode 1 second of video */
+    for (i = 0; i < 25; i++) {
+        fflush(stdout);
+
+        /* make sure the frame data is writable */
+        ret = av_frame_make_writable(picture);
+        if (ret < 0)
+            exit(1);
+
+        /* prepare a dummy image */
+        /* Y */
+        for (y = 0; y < c->height; y++) {
+            for (x = 0; x < c->width; x++) {
+                picture->data[0][y * picture->linesize[0] + x] = x + y + i * 3;
+            }
+        }
+
+        /* Cb and Cr */
+        for (y = 0; y < c->height / 2; y++) {
+            for (x = 0; x < c->width / 2; x++) {
+                picture->data[1][y * picture->linesize[1] + x] = 128 + y + i * 2;
+                picture->data[2][y * picture->linesize[2] + x] = 64 + x + i * 5;
+            }
+        }
+
+        picture->pts = i;
+
+        /* encode the image */
+        encode(c, picture, pkt, f);
+    }
+
+    /* flush the encoder */
+    encode(c, nullptr, pkt, f);
+
+    /* add sequence end code to have a real MPEG file */
+    fwrite(end_code, 1, sizeof(end_code), f);
+    fclose(f);
+
+    avcodec_free_context(&c);
+    av_frame_free(&picture);
+    av_packet_free(&pkt);
+
+    return 0;
+}
