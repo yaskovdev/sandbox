edb7ca027b3f92c7c6a65a974a392f4e4eae78c4
yaskovdev <yaskovdev@gmail.com>
2021-12-22 18:02:17 +0200

    Commit latest changes.

---
+++ ffmpeg-muxing/cmake-build-debug/CMakeFiles/ffmpeg_muxing.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ffmpeg_muxing.dir/main.cpp.o"
+  "ffmpeg_muxing"
+  "ffmpeg_muxing.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ffmpeg_muxing.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
