2f90f5d6564409dc7da26153e3c1c73bae181270
yaskovdev <yaskovdev@gmail.com>
2021-12-24 18:56:05 +0200

    Clarify how to use strings.

--- cpp-sandbox/main.cpp
+++ cpp-sandbox/main.cpp
@@ -16,28 +16,27 @@ bool function_that_returns_bool() {
     return !!status;
 }
 
+// Allocate on the stack if possible. It is not possible if you need a bigger scope, or you need to load really much data (megabytes of data).
 int main(int argc, char **argv) {
-    if (function_that_returns_bool())
-        std::cout << "true" << std::endl;
-    else
-        std::cout << "false" << std::endl;
-//    for (int i = 0; i < 10000; i++) {
-////        std::string file_name = generate_file_name("/Users/yaskovdev/dev/frames/");
-////        const long long ms = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
-////        std::stringstream stream;
-////        stream << "/Users/yaskovdev/dev/frames/" << ms;
-////        const std::string string = stream.str();
-////        const char *file_name = string.c_str();
-////        std::cout << "File name is " << file_name << std::endl;
-//        char *word = new char[10];
-//        for (int j = 0; j < 10; ++j) {
-//            word[j] = 'a';
-//        }
-//        printf("Word is %s\n", word);
-////        FILE *f = fopen(file_name, "wb");
-////        fwrite(word, 1, 10, f);
-////        fclose(f);
-//        delete[] word;
-//    }
+    for (int i = 0; i < 10000; i++) {
+        int word_size = 10;
+        char *heap_word = new char[word_size];
+        for (int j = 0; j < word_size; ++j) {
+            heap_word[j] = 'a';
+        }
+        printf("Heap word is %.*s\n", word_size, heap_word);
+        printf("Heap word is %.10s\n", heap_word);
+        delete[] heap_word;
+
+        char *null_terminated_heap_word = new char[word_size + 1];
+        for (int j = 0; j < word_size; ++j) {
+            null_terminated_heap_word[j] = 'a';
+        }
+        null_terminated_heap_word[word_size] = '\0';
+        printf("Null-terminated heap word is %s\n", null_terminated_heap_word);
+
+        const char *string_literal_data_segment_word = "aaaaaaaaaa"; // string literals are stored in data segment: https://en.wikipedia.org/wiki/Data_segment
+        printf("String literal heap word is %s\n", string_literal_data_segment_word);
+    }
     return 0;
 }
