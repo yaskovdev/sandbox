b539cf0ef71a0154b354c21be04f1bd8f846f8a3
yaskovdev <yaskovdev@gmail.com>
2021-12-24 20:35:31 +0200

    Clarify how to return structs.

--- cpp-sandbox/main.cpp
+++ cpp-sandbox/main.cpp
@@ -8,12 +8,24 @@ static std::string generate_file_name(const char *directory) {
     const long long ms = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
     std::stringstream stream;
     stream << directory << ms;
-    return stream.str();
+    std::string answer = stream.str();
+    const char *name = answer.c_str();
+    return name;
 }
 
 bool function_that_returns_bool() {
     int status = 0;
     return !!status;
+}
+
+typedef struct pair {
+    int x;
+    int y;
+} pair;
+
+pair function_that_returns_pair_by_value() {
+    pair p = {0, 1};
+    return p;
 }
 
 // Allocate on the stack if possible. It is not possible if you need a bigger scope, or you need to load really much data (megabytes of data).
@@ -37,6 +49,13 @@ int main(int argc, char **argv) {
 
         const char *string_literal_data_segment_word = "aaaaaaaaaa"; // string literals are stored in data segment: https://en.wikipedia.org/wiki/Data_segment
         printf("String literal heap word is %s\n", string_literal_data_segment_word);
+    }
+
+    for (int i = 0; i < 100000; i++) {
+        const pair p = function_that_returns_pair_by_value();
+        const pair q = {10, 20};
+        printf("p.x=%d, p.y=%d\n", p.x, p.y);
+        printf("q.x=%d, q.y=%d\n", q.x, q.y);
     }
     return 0;
 }
