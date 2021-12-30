47231dfa58659738a040c1bc7029be5cf06cc806
yaskovdev <yaskovdev@gmail.com>
2021-12-30 23:57:31 +0200

    - Render real full screen. - Do not show cursor.

--- cpp-space-game/main.cpp
+++ cpp-space-game/main.cpp
@@ -62,18 +62,22 @@ long long now_us() {
 
 int main(int const argc, char const *const argv[]) {
     SDL_Init(SDL_INIT_VIDEO);
-    SDL_Window *const window = SDL_CreateWindow("Game", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 0, 0, SDL_WINDOW_FULLSCREEN_DESKTOP);
+    SDL_DisplayMode display_mode;
+    int status = SDL_GetCurrentDisplayMode(0, &display_mode);
+    if (status < 0) {
+        cout << SDL_GetError() << endl;
+        return status;
+    }
+    SDL_ShowCursor(SDL_DISABLE);
+    int w = display_mode.w;
+    int h = display_mode.h;
+    SDL_Window *const window = SDL_CreateWindow("Game", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, w, h, SDL_WINDOW_FULLSCREEN);
     SDL_Renderer *const renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-
-    int w;
-    int h;
-    SDL_GetWindowSize(window, &w, &h);
     std::random_device os_seed;
     const uint_least32_t seed = os_seed();
     std::mt19937 generator(seed);
-    pair field_size = pair(w, h);
     class clock c;
-    game g(c, generator, field_size);
+    game g(c, generator, pair(w, h));
 
     const long long dt = 10000;
     long long current_time = now_us();
