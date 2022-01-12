28c88368560f22bbcd9878208a761250a8f9c296
Sergei Iaskov <sergeiiaskov@microsoft.com>
2022-01-12 20:14:18 +0200

    Commit local changes.

---
+++ CsSandbox/CsSandbox/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CsSandbox
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ DotNetSandbox/ReferenceVsImplementationAssembly/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+
+namespace ReferenceVsImplementationAssembly
+{
+    static class Program
+    {
+        private static void Main()
+        {
+            throw new ConfigurationErrorsException("Cannot parse configuration setting");
+        }
+    }
+}
+
