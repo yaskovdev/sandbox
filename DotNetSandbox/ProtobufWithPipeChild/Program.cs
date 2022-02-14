53f2fc19a86a8e8bcbb90638128d4d2e8a103288
Sergei Iaskov <sergeiiaskov@microsoft.com>
2022-02-14 12:27:10 +0200

    Simplify even more.

--- DotNetSandbox/ProtobufWithPipeChild/Program.cs
+++ DotNetSandbox/ProtobufWithPipeChild/Program.cs
@@ -11,7 +11,7 @@ public static class Program
         using var pipeReader = new StreamReader(parentToChildPipe);
         try
         {
-            Serializer.DeserializeWithLengthPrefix<Command>(parentToChildPipe, PrefixStyle.Base128); // if replace Command with string, then stops reproducing
+            Serializer.DeserializeWithLengthPrefix<int>(parentToChildPipe, PrefixStyle.Base128); // if replace int with string, then stops reproducing
         }
         catch (Exception e)
         {
