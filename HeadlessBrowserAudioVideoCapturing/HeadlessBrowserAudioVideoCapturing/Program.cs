b99b06819517cf726fc4b6e6ea3d40c9e0fb85ec
yaskovdev <yaskovdev@gmail.com>
2022-03-03 19:52:27 +0200

    Example.

--- HeadlessBrowserAudioVideoCapturing/HeadlessBrowserAudioVideoCapturing/Program.cs
+++ HeadlessBrowserAudioVideoCapturing/HeadlessBrowserAudioVideoCapturing/Program.cs
@@ -1,9 +1,20 @@
-﻿namespace HeadlessBrowserAudioVideoCapturing;
+﻿using CefSharp.OffScreen;
+
+namespace HeadlessBrowserAudioVideoCapturing;
 
 public static class Program
 {
-    public static void Main()
+    public static async Task Main()
     {
-        Console.WriteLine("Hello, World!");
+        using var browser = new ChromiumWebBrowser("https://www.youtube.com/watch?v=tPEE9ZwTmy0");
+        var initialLoadResponse = await browser.WaitForInitialLoadAsync();
+        if (!initialLoadResponse.Success)
+        {
+            throw new Exception($"Page load failed with ErrorCode:{initialLoadResponse.ErrorCode}, HttpStatusCode:{initialLoadResponse.HttpStatusCode}");
+        }
+        await Task.Delay(500);
+        var bitmapAsByteArray = await browser.CaptureScreenshotAsync();
+        var screenshotPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "CefSharp_screenshot.png");
+        Console.WriteLine("Screenshot ready. Saving to {0}", screenshotPath);
     }
 }
