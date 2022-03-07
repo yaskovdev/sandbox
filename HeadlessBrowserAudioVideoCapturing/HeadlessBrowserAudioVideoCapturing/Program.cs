6332e5292f62ba10263e2cb98448eeea8f023cad
Sergei Iaskov <sergeiiaskov@microsoft.com>
2022-03-07 17:19:46 +0200

    Use AsyncContext from Nito.AsyncEx.Context instead of custom one.

--- HeadlessBrowserAudioVideoCapturing/HeadlessBrowserAudioVideoCapturing/Program.cs
+++ HeadlessBrowserAudioVideoCapturing/HeadlessBrowserAudioVideoCapturing/Program.cs
@@ -1,12 +1,13 @@
 ﻿using CefSharp;
 using CefSharp.DevTools.Page;
 using CefSharp.OffScreen;
+using Nito.AsyncEx;
 
 namespace HeadlessBrowserAudioVideoCapturing;
 
 public static class Program
 {
-    public static async Task Main()
+    public static void Main()
     {
         AsyncContext.Run(async delegate
         {
@@ -17,22 +18,25 @@ public static class Program
             using var browser = new ChromiumWebBrowser("https://yaskovdev.github.io/video-and-audio-capturing-test/");
             browser.AudioHandler = new CustomAudioHandler();
             var initialLoadResponse = await browser.WaitForInitialLoadAsync();
-            if (!initialLoadResponse.Success)
-            {
-                throw new Exception($"Page load failed with ErrorCode:{initialLoadResponse.ErrorCode}, HttpStatusCode:{initialLoadResponse.HttpStatusCode}");
-            }
+            AssertSuccess(initialLoadResponse.Success,
+                $"Page load failed with ErrorCode:{initialLoadResponse.ErrorCode}, HttpStatusCode:{initialLoadResponse.HttpStatusCode}");
             var devToolsClient = browser.GetDevToolsClient();
             var page = devToolsClient.Page;
-            page.ScreencastFrame += async (sender, args) =>
+            page.ScreencastFrame += async (_, args) =>
             {
                 await page.ScreencastFrameAckAsync(args.SessionId);
-                Console.WriteLine($"Got frame with session ID {args.SessionId}");
-                await File.WriteAllBytesAsync(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "frames", $"capture_{DateTime.UtcNow.Ticks}.png"), args.Data);
+                Console.WriteLine($"Got frame with session ID {args.SessionId} and timestamp {args.Metadata.Timestamp}");
+                var path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "frames", $"capture_{DateTime.UtcNow.Ticks}.png");
+                await File.WriteAllBytesAsync(path, args.Data);
             };
             var response = await page.StartScreencastAsync(StartScreencastFormat.Png, 100, 800, 600, 1);
-            Console.WriteLine($"Response is {response.ResponseAsJsonString}");
-
+            AssertSuccess(response.Success, $"Cannot start screencast, DevTools response is {response.ResponseAsJsonString}");
             Console.ReadLine();
         });
+    }
+
+    private static void AssertSuccess(bool success, string errorMessage)
+    {
+        if (!success) throw new Exception(errorMessage);
     }
 }
