f7257c3fd5078d5103b7f235e02ac267f23507ad
yaskovdev <yaskovdev@gmail.com>
2022-09-05 00:23:20 +0300

    Cleanup.

--- StatefulService/StatefulServiceProblem/StatefulSessionEventHandler.cs
+++ StatefulService/StatefulServiceProblem/StatefulSessionEventHandler.cs
@@ -3,7 +3,7 @@ namespace StatefulServiceProblem;
 /// <summary>
 /// Contains both state and business logic (average duration calculation), just like OOP teaches us, right?
 /// But cannot use DI to instantiate this class, have to instantiate by myself. If there is a tree of such classes,
-/// have to create the whole tree by myself. Just imagine that the class needs 50 more dependencies to work handle
+/// have to create the whole tree by myself. Just imagine that the class needs 50 more dependencies to handle
 /// events properly.
 /// TODO: consider writing an article about this problem.
 /// Note: APIs of both the services are the same. But they are implemented differently.
@@ -14,7 +14,7 @@ public class StatefulSessionEventHandler : ISessionEventHandler
 
     private int NumberOfEvents { get; set; }
 
-    public double AverageDurationOfEvents { get; set; }
+    public double AverageDurationOfEvents { get; private set; }
 
     public void HandleEvent(SessionEvent sessionEvent)
     {
