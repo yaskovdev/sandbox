9cf1f4bc95655d0c71626353dd54904237d36cb3
Sergei Iaskov <sergeiiaskov@microsoft.com>
2022-09-10 13:58:45 +0300

    Cleanup.

--- DotNetCppSandbox/DotNetCppSandbox/Program.cs
+++ DotNetCppSandbox/DotNetCppSandbox/Program.cs
@@ -10,11 +10,11 @@ internal static class Program
         const int coefficient = 2;
         const int number = 5;
 
-        TryIntPtrMultiplier(coefficient, number);
-        TrySafeHandleMultiplier(coefficient, number);
+        UseIntPtrMultiplier(coefficient, number);
+        UseSafeHandleMultiplier(coefficient, number);
     }
 
-    private static void TryIntPtrMultiplier(int coefficient, int number)
+    private static void UseIntPtrMultiplier(int coefficient, int number)
     {
         var multiplier = MultiplierIntPtrApi.CreateMultiplierExtern(coefficient);
         try
@@ -28,11 +28,11 @@ internal static class Program
         }
     }
 
-    private static void TrySafeHandleMultiplier(int coefficient, int number)
+    private static void UseSafeHandleMultiplier(int coefficient, int number)
     {
         var api = new MultiplierSafeHandleApi();
         using var multiplier = api.CreateMultiplier(coefficient);
-        Console.WriteLine($"Multiplier SafeHandle created at address {multiplier}");
+        Console.WriteLine($"Multiplier SafeHandle created");
         Console.WriteLine($"{number} multiplied by {coefficient} is {api.Multiply(multiplier, number)}");
     }
 }
