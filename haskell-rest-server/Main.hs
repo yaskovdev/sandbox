27c461b3560495e62c05973445893c519f1234f4
yaskovdev <yaskovdev@gmail.com>
2022-10-30 15:03:29 +0200

    New sandbox.

--- haskell-rest-server/Main.hs
+++ haskell-rest-server/Main.hs
@@ -1,4 +1,10 @@
-module Main where
+{-# LANGUAGE OverloadedStrings #-}
 
-main :: IO ()
-main = putStrLn "Hello, Haskell!"
+import Web.Scotty
+
+import Data.Monoid (mconcat)
+
+main = scotty 3000 $ do
+  get "/:word" $ do
+    beam <- param "word"
+    html $ mconcat ["<h1>Scotty, ", beam, " me up!</h1>"]
---
+++ haskell-stack-rest-server/README.md
@@ -0,0 +1,1 @@
+# haskell-stack-rest-server
