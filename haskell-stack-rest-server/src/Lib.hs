248ab94897577ab7fff817f64619f5fb69fc50ce
yaskovdev <yaskovdev@gmail.com>
2022-12-19 13:54:51 +0200

    Commit uncommitted.

--- haskell-stack-rest-server/src/Lib.hs
+++ haskell-stack-rest-server/src/Lib.hs
@@ -1,6 +1,4 @@
 module Lib (response) where
 
-import qualified Data.Text.Lazy as L
-
-response :: String -> L.Text
-response beam = L.pack (concat ["<h1>Scotty, ", beam, " me up!</h1>"])
+response :: String -> String
+response beam = concat ["<h1>Scotty, ", beam, " me up!</h1>"]
