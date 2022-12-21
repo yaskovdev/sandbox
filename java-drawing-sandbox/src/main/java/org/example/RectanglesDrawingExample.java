d480abff56c956aa65a6d28c92ee45b7bccd7d14
yaskovdev <yaskovdev@gmail.com>
2022-12-21 18:23:38 +0200

    More or less working version.

--- java-drawing-sandbox/src/main/java/org/example/RectanglesDrawingExample.java
+++ java-drawing-sandbox/src/main/java/org/example/RectanglesDrawingExample.java
@@ -1,40 +1,69 @@
 package org.example;
 
 
-import java.awt.*;
-import java.awt.event.*;
+import lombok.SneakyThrows;
+import org.spiderland.Psh.BooleanStack;
+import org.spiderland.Psh.FloatStack;
+import org.spiderland.Psh.Interpreter;
+import org.spiderland.Psh.ObjectStack;
+import org.spiderland.Psh.Program;
+
 import javax.swing.*;
+import java.awt.*;
 
 public class RectanglesDrawingExample extends JFrame {
-    private static final int D_W = 500;
+    private static final int D_W = 2000;
     private static final int D_H = 200;
     private static final int CART_WIDTH = 50;
     private static final int CART_HEIGHT = 50;
-    int x = 0;
+
+    private final Interpreter interpreter = new Interpreter();
+
+    // TODO: maybe vice versa
+    private float cartPosition = -1;
+    private float cartVelocity = -1.0f;
+
+    private float cartAcceleration = 0.5f;
+
+    int time = 0;
+
     int y = 100;
 
     DrawPanel drawPanel = new DrawPanel();
 
+    @SneakyThrows
     public RectanglesDrawingExample() {
-        ActionListener listener = new AbstractAction() {
-            public void actionPerformed(ActionEvent e) {
-                if (x >= D_W) {
-                    x = 0;
-                    drawPanel.repaint();
-                } else {
-                    x += 10;
-                    drawPanel.repaint();
-                }
-            }
-        };
-        Timer timer = new Timer(100, listener);
-        timer.start();
-        add(drawPanel);
+        final Program program = new Program(interpreter, "(float.% float.neg 8.789999 input.in1 float.% float.% input.inallrev float.+ float.>)");
 
+        add(drawPanel);
         pack();
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
         setVisible(true);
+        Thread thread = new Thread(() -> {
+            while (time < 1000) {
+                interpreter.ClearStacks();
+                final FloatStack floatStack = interpreter.floatStack();
+                final ObjectStack inputStack = interpreter.inputStack();
+                final BooleanStack booleanStack = interpreter.boolStack();
+                floatStack.push(cartPosition);
+                floatStack.push(cartVelocity);
+                inputStack.push(cartPosition);
+                inputStack.push(cartVelocity);
+                interpreter.Execute(program, 150);
+                final boolean positiveForce = booleanStack.pop();
+                cartVelocity += (positiveForce ? 1 : -1) * cartAcceleration;
+                cartPosition += cartVelocity / 1000;
+                drawPanel.repaint();
+                try {
+                    Thread.sleep(40);
+                } catch (Exception ignored) {
+
+                }
+                time++;
+            }
+        });
+        thread.start();
     }
 
     private class DrawPanel extends JPanel {
@@ -42,7 +71,7 @@ public class RectanglesDrawingExample extends JFrame {
         protected void paintComponent(Graphics g) {
             super.paintComponent(g);
             g.setColor(Color.BLACK);
-            g.fillRect(x, y, CART_WIDTH, CART_HEIGHT);
+            g.fillRect((int) (cartPosition * 1000) + 1000, y, CART_WIDTH, CART_HEIGHT);
             g.setColor(Color.GRAY);
             g.fillRect(0, y + CART_HEIGHT, D_W, 10);
         }
