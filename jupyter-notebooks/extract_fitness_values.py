3f0d07e3e08c4c65cddb1b5fe690933ef3633912
yaskovdev <yaskovdev@gmail.com>
2022-12-25 15:33:47 +0200

    Move to file reading logic to the notebook.

--- jupyter-notebooks/extract_fitness_values.py
+++
@@ -1,4 +0,0 @@
-with open("/Users/yaskovdev/Yandex.Disk.localized/gp/gp_robot_evolution_log_2.txt") as file:
-    lines = [line.rstrip() for line in file]
-    fitness_values = [float(v.split(" ")[5]) for v in lines if v.startswith(";; Best Program Fitness (mean):")]
-    print(fitness_values)
