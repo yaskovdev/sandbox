58a2893808768b9a8413d7871f519b99c3ee331a
Sergei Iaskov <sergeiiaskov@microsoft.com>
2023-05-16 22:35:55 +0300

    Add a benchmark.

---
+++ DotNetCppSandbox/FillArrayBenchmark/FillArrayApi.cs
@@ -0,0 +1,12 @@
+﻿namespace FillArrayBenchmark;
+
+using System.Runtime.InteropServices;
+
+internal static class FillArrayApi
+{
+    [DllImport("NativeLibrary.dll", EntryPoint = "fill_array", CallingConvention = CallingConvention.Cdecl)]
+    internal static extern void FillArray(byte[] array, int length);
+
+    [DllImport("NativeLibrary.dll", EntryPoint = "fill_array", CallingConvention = CallingConvention.Cdecl)]
+    internal static extern void FillArrayFixed([Out, MarshalAs(UnmanagedType.LPArray, SizeParamIndex = 1)] byte[] array, int length);
+}
