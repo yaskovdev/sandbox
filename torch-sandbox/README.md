a7427e03e1541922d468929b366ac3a063bdfe98
Sergey Yaskov <yaskovdev@gmail.com>
2024-01-07 00:43:44 +0200

    Cleanup.

--- torch-sandbox/README.md
+++ torch-sandbox/README.md
@@ -5,6 +5,6 @@
    Latest PyTorch requires Python 3.8 or later", the latest Python may be not supported. Pick one that is definitely
    supported.
 3. Activate the new environment with `conda activate torch`.
-4. Follow [this guide](https://pytorch.org/) to install PyTorch.
+4. Follow [this guide](https://pytorch.org/get-started/locally/) to install PyTorch.
 5. If you are using PyCharm, make sure to [pick the Conda environment](https://stackoverflow.com/a/46133678/1862286) you
    have just created.
--- torch-sandbox/main.py
+++ torch-sandbox/main.py
@@ -1,19 +1,12 @@
 import torch
 
 
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def assert_gpu_available():
+    assert torch.cuda.is_available(), "GPU is not available"
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {torch.cuda.is_available()}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    assert_gpu_available()
+    print("Ready to go")
+    x = torch.rand(2, 3)
+    print(x)
