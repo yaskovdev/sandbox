460747e74e6f1fb2f9ff805f5670da85446fb93d
Sergei Iaskov <sergeiiaskov@microsoft.com>
2024-01-09 22:18:44 +0200

    Cleanup.

--- torch-sandbox/main.py
+++ torch-sandbox/main.py
@@ -26,32 +26,6 @@ def load_training_data():
     return extract_song_snippet(text)
 
 
-def test_batch_func_types(func, args):
-    ret = func(*args)
-    assert len(ret) == 2, "[FAIL] get_batch must return two arguments (input and label)"
-    assert type(ret[0]) == np.ndarray, "[FAIL] test_batch_func_types: x is not np.array"
-    assert type(ret[1]) == np.ndarray, "[FAIL] test_batch_func_types: y is not np.array"
-    print("[PASS] test_batch_func_types")
-    return True
-
-
-def test_batch_func_shapes(func, args):
-    dataset, seq_length, batch_size = args
-    x, y = func(*args)
-    correct = (batch_size, seq_length)
-    assert x.shape == correct, "[FAIL] test_batch_func_shapes: x {} is not correct shape {}".format(x.shape, correct)
-    assert y.shape == correct, "[FAIL] test_batch_func_shapes: y {} is not correct shape {}".format(y.shape, correct)
-    print("[PASS] test_batch_func_shapes")
-    return True
-
-
-def test_batch_func_next_step(func, args):
-    x, y = func(*args)
-    assert (x[:, 1:] == y[:, :-1]).all(), "[FAIL] test_batch_func_next_step: x_{t} must equal y_{t-1} for all t"
-    print("[PASS] test_batch_func_next_step")
-    return True
-
-
 def vectorize_string(string, character_to_index):
     return np.array([character_to_index[character] for character in string])
 
@@ -114,16 +88,6 @@ if __name__ == '__main__':
     print(vectorized_songs)
 
     print('{} ---- characters mapped to int ----> {}'.format(repr(songs_joined[:10]), vectorized_songs[:10]))
-
-    assert isinstance(vectorized_songs, np.ndarray), "returned result should be a numpy array"
-
-    test_args = (vectorized_songs, 10, 2)
-    if not test_batch_func_types(get_batch, test_args) or \
-            not test_batch_func_shapes(get_batch, test_args) or \
-            not test_batch_func_next_step(get_batch, test_args):
-        print("======\n[FAIL] could not pass tests")
-    else:
-        print("======\n[PASS] passed all tests!")
 
     x_batch, y_batch = get_batch(vectorized_songs, seq_length=5, batch_size=1)
 
