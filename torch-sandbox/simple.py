1b65bd096b32bb62e67f789dffdf0cab56d3fa4e
Sergey Yaskov <yaskovdev@gmail.com>
2024-05-06 18:01:26 +0300

    Use test data from 3Blue1Brown to train a simpler network that recognizes digits.

--- torch-sandbox/simple.py
+++ torch-sandbox/simple.py
@@ -1,17 +1,18 @@
-import os
+import math
 
 import keras_core as keras
 import numpy as np
-import regex as re
-import torch
-from torch.nn.functional import log_softmax, softmax
+from PIL import Image
+
+import mnist_loader
 
 
-# https://www.kaggle.com/code/ryanholbrook/a-single-neuron/tutorial
 def build_model():
     return keras.Sequential([
-        keras.layers.Input(shape=(3,)),
-        keras.layers.Dense(units=1)
+        keras.layers.Input(shape=(784,)),
+        keras.layers.Dense(units=16),
+        keras.layers.Dense(units=16),
+        keras.layers.Dense(units=10)
     ])
 
 
@@ -20,3 +21,8 @@ print("Ready to go")
 model = build_model()
 
 model.summary()
+
+data = mnist_loader.load_data()
+image_index = 2
+image = data[image_index][0][0]
+Image.fromarray((255 * np.reshape(image, (math.isqrt(image.size), -1))).astype('uint8')).show()
