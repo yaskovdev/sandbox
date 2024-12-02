8be18b9f65ddc4b6192c0317b1aa8c31169e8688
Sergey Yaskov <yaskovdev@gmail.com>
2024-12-02 23:30:14 +0200

    Implement support for nested parentheses in Universal Register Machine interpreter.

--- UniversalRegisterMachineInterpreter/UniversalRegisterMachineInterpreter/Program.cs
+++ UniversalRegisterMachineInterpreter/UniversalRegisterMachineInterpreter/Program.cs
@@ -1,8 +1,8 @@
 ﻿// See https://iwriteiam.nl/Ha_bf_Turing.html for more information
 
-var code = "(a2;s3)3.";
+var code = "(a4;a5;s2)2; ((a2;s4)4; s3; (a1;a4;s5)5; (a5;s1)1)3.";
 var registers = new int[100];
-registers[2 - 1] = 1;
+registers[2 - 1] = 6;
 registers[3 - 1] = 3;
 Console.WriteLine("Before: " + string.Join(", ", registers));
 var pointer = 0;
@@ -20,9 +20,19 @@ while (code[pointer] != '.')
     }
     else if (code[pointer] == '(')
     {
-        while (code[pointer] != ')')
+        var b = 1;
+        while (b > 0)
         {
             pointer++;
+            switch (code[pointer])
+            {
+                case '(':
+                    b++;
+                    break;
+                case ')':
+                    b--;
+                    break;
+            }
         }
 
         pointer--;
@@ -32,9 +42,19 @@ while (code[pointer] != '.')
         var register = registers[code[pointer + 1] - '0' - 1];
         if (register > 0)
         {
-            while (code[pointer] != '(')
+            var b = 1;
+            while (b > 0)
             {
                 pointer--;
+                switch (code[pointer])
+                {
+                    case ')':
+                        b++;
+                        break;
+                    case '(':
+                        b--;
+                        break;
+                }
             }
         }
         else
