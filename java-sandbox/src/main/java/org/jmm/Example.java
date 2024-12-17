0a212d63229627e4060d63b3064e90c37616806e
Sergey Yaskov <yaskovdev@gmail.com>
2024-12-17 12:01:11 +0200

    Cleanup.

--- java-sandbox/src/main/java/org/jmm/Example.java
+++ java-sandbox/src/main/java/org/jmm/Example.java
@@ -1,7 +1,7 @@
 package org.jmm;
 
 /**
- * If I run method1 and method2 concurrently, is it possible to see two 0 printed to the console?
+ * If I run run1 and run2 concurrently, is it possible to see two 0 printed to the console?
  */
 public class Example {
 
