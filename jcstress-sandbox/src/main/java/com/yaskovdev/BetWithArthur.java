e16f12db9655c51b8d4c9f4aca9bfe58ca37bed1
Sergey Yaskov <yaskovdev@gmail.com>
2024-12-20 16:42:54 +0200

    Add one more test.

---
+++ jcstress-sandbox/src/main/java/com/yaskovdev/BetWithArthur.java
@@ -0,0 +1,43 @@
+package com.yaskovdev;
+
+
+import org.openjdk.jcstress.annotations.*;
+import org.openjdk.jcstress.infra.results.II_Result;
+
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.VarHandle;
+
+@JCStressTest
+@Outcome(id = "1, 0", expect = Expect.ACCEPTABLE_INTERESTING)
+@State
+public class BetWithArthur {
+    private static final VarHandle VH_X, VH_Y;
+
+    static {
+        try {
+            VH_X = MethodHandles.lookup().findVarHandle(BetWithArthur.class,
+                    "x", int.class);
+            VH_Y = MethodHandles.lookup().findVarHandle(BetWithArthur.class,
+                    "y", int.class);
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    int x, y;
+
+    @Actor
+    void actor() {
+        synchronized (this) {
+            x = 1;
+            y = 1;
+        }
+    }
+
+    @Actor
+    void observer(II_Result r) {
+        r.r1 = (int) VH_Y.getOpaque(this);
+        r.r2 = (int) VH_X.getOpaque(this);
+    }
+}
+
