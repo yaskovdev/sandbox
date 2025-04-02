67f6cef0ea1a1c0402d74c8c356cef5c457ed4ab
Sergey Yaskov <yaskovdev@gmail.com>
2025-04-03 00:07:43 +0300

    Commit the latest changes.

--- AzureStorageBlobsMetadata/AzureStorageBlobsMetadata/BlobWriter.cs
+++ AzureStorageBlobsMetadata/AzureStorageBlobsMetadata/BlobWriter.cs
@@ -1,27 +1,32 @@
 namespace AzureStorageBlobsMetadata;
 
-using System.Collections.Immutable;
-using Azure.Storage.Blobs.Models;
 using Azure.Storage.Blobs.Specialized;
 
 public class BlobWriter(AppendBlobClient blob)
 {
-    public async Task Start()
+    public async Task Start(string id)
     {
         Stream? stream = await blob.OpenWriteAsync(false, null, CancellationToken.None);
         var payload = new byte[4 * 1024 * 1024]; // 2 MB
         var random = new Random();
-        for (var i = 0; i < 1000; i++)
+        try
         {
-            random.NextBytes(payload);
-            Console.WriteLine($"Writing {payload.Length} bytes to blob stream");
-            var readOnlyMemory = new ReadOnlyMemory<byte>(payload);
-            var cancellationToken = CancellationToken.None;
-            await stream.WriteAsync(readOnlyMemory, cancellationToken);
-            var keepalive = Guid.NewGuid().ToString();
-            Console.WriteLine($"Writing keepalive {keepalive} to metadata");
-            await blob.SetMetadataAsync(ImmutableDictionary<string, string>.Empty.Add("keepalive", "123"));
-            await Task.Delay(TimeSpan.FromSeconds(1));
+            for (var i = 0; i < 1000; i++)
+            {
+                random.NextBytes(payload);
+                Console.WriteLine($"Writing {payload.Length} bytes to blob stream from {id}");
+                var readOnlyMemory = new ReadOnlyMemory<byte>(payload);
+                var cancellationToken = CancellationToken.None;
+                await stream.WriteAsync(readOnlyMemory, cancellationToken);
+                // var keepalive = Guid.NewGuid().ToString();
+                // Console.WriteLine($"Writing keepalive {keepalive} to metadata");
+                // await blob.SetMetadataAsync(ImmutableDictionary<string, string>.Empty.Add("keepalive", "123"));
+                await Task.Delay(TimeSpan.FromSeconds(1));
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Error writing to blob stream in {id}: {e}");
         }
     }
 }
