e81693f8a562916dfb0fca295fafda209748cb90
Sergei Iaskov <sergeiiaskov@microsoft.com>
2025-04-17 11:50:41 +0300

    Change the buffer size, since, apparently, it's divisibility by 4 that matters.

--- HeapCorruptionSandbox/NativeLibrary/api.cpp
+++ HeapCorruptionSandbox/NativeLibrary/api.cpp
@@ -3,8 +3,8 @@
 
 void corrupt_heap()
 {
-    int* arr = new int[96];
-    arr[96] = 42;
+    int* arr = new int[4];
+    arr[4] = 42;
     std::cout << "Heap corrupted, but no crash yet\n";
     delete[] arr;
     std::cout << "Deleted arr\n";
