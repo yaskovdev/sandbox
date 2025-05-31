db56a1ecfdfc475897fc0e4bae5d2ec84fe664cc
Sergey Yaskov <yaskovdev@gmail.com>
2025-05-31 21:28:39 +0300

    Refactor SessionService to improve Redis integration and add session update functionality

--- IdempotentRetriesSandbox/IdempotentRetriesSandbox/Session.cs
+++ IdempotentRetriesSandbox/IdempotentRetriesSandbox/Session.cs
@@ -1,29 +1,23 @@
 namespace IdempotentRetriesSandbox;
 
-using System.Text.Json;
 using Polly.Contrib.Simmy;
 using Polly.Contrib.Simmy.Outcomes;
-using StackExchange.Redis;
 
 // ReSharper disable once ClassNeverInstantiated.Global, this is instantiated by the factory
 public class Session : IDisposable
 {
     private readonly string _id;
-    private readonly IConnectionMultiplexer _redis;
     private readonly ILogger<Session> _logger;
-    private readonly Timer _timer;
     private readonly InjectOutcomePolicy _chaosPolicy;
     private int _disposed;
 
-    public Session(string id, IConnectionMultiplexer redis, ILogger<Session> logger)
+    public Session(string id, ILogger<Session> logger)
     {
         _id = id;
-        _redis = redis;
         _logger = logger;
-        _timer = new Timer(UpdateSession, id, TimeSpan.Zero, TimeSpan.FromSeconds(3));
         _chaosPolicy = MonkeyPolicy.InjectException(with =>
             with.Fault(new InvalidOperationException("Cannot start session, Media Platform is down"))
-                .InjectionRate(0.5)
+                .InjectionRate(0.3)
                 .Enabled(true)
         );
         Start();
@@ -38,26 +32,9 @@ public class Session : IDisposable
     {
         if (Interlocked.CompareExchange(ref _disposed, 1, 0) == 0)
         {
-            _timer.Dispose();
+            _logger.LogInformation("Session with ID: {SessionId} disposed", _id);
         }
 
         GC.SuppressFinalize(this);
-    }
-
-    private void UpdateSession(object? state)
-    {
-        var sessionId = (string)state;
-        _logger.LogInformation("Updating session with ID: {SessionId}", sessionId);
-        var database = _redis.GetDatabase();
-        var value = database.StringGet(sessionId);
-        if (value.IsNull)
-            return;
-
-        var session = JsonSerializer.Deserialize<SessionEntity>((string)value);
-        if (session == null)
-            return;
-
-        var updatedSession = session with { UpdatedAt = DateTime.Now };
-        database.StringSet(sessionId, JsonSerializer.Serialize(updatedSession));
     }
 }
--- IdempotentRetriesSandbox/IdempotentRetriesSandbox/SessionService.cs
+++ IdempotentRetriesSandbox/IdempotentRetriesSandbox/SessionService.cs
@@ -1,23 +1,36 @@
 namespace IdempotentRetriesSandbox;
 
 using System.Collections.Concurrent;
+using System.Collections.Immutable;
 using System.Text.Json;
 using StackExchange.Redis;
 
-public class SessionService(IConnectionMultiplexer redis, ISessionFactory sessionFactory) : ISessionService
+public class SessionService : ISessionService, IAsyncDisposable
 {
     private readonly string _setIfNotExistsElseGet = ReadResource("SetIfNotExistsElseGet.lua");
     private readonly ConcurrentDictionary<string, Session> _sessions = new();
+    private readonly IConnectionMultiplexer _redis;
+    private readonly ISessionFactory _sessionFactory;
+    private readonly ILogger<SessionService> _logger;
+    private readonly Timer _timer;
+
+    public SessionService(IConnectionMultiplexer redis, ISessionFactory sessionFactory, ILogger<SessionService> logger)
+    {
+        _redis = redis;
+        _sessionFactory = sessionFactory;
+        _logger = logger;
+        _timer = new Timer(UpdateSessions, _sessions, TimeSpan.Zero, TimeSpan.FromSeconds(3));
+    }
 
     public SessionEntity CreateSession(string sessionId)
     {
-        var database = redis.GetDatabase();
+        var database = _redis.GetDatabase();
         var now = DateTime.Now;
         var newProcessing = new SessionEntity(now, now);
         var newProcessingJson = JsonSerializer.Serialize(newProcessing);
         var result = database.ScriptEvaluate(_setIfNotExistsElseGet, [sessionId], [newProcessingJson]);
         var sessionEntity = result.IsNull ? newProcessing : JsonSerializer.Deserialize<SessionEntity>((string)result);
-        var session = sessionFactory.CreateSession(sessionId);
+        var session = _sessionFactory.CreateSession(sessionId);
         if (!_sessions.TryAdd(sessionId, session))
         {
             session.Dispose();
@@ -31,10 +44,42 @@ public class SessionService(IConnectionMultiplexer redis, ISessionFactory sessio
         if (_sessions.TryRemove(sessionId, out var session))
         {
             session.Dispose();
-            var database = redis.GetDatabase();
+            var database = _redis.GetDatabase();
             // TODO: probably should change state to Finished instead of deleting the session
             database.KeyDelete(sessionId);
         }
+    }
+
+    public async ValueTask DisposeAsync()
+    {
+        await _timer.DisposeAsync();
+        GC.SuppressFinalize(this);
+    }
+
+    private void UpdateSessions(object? state)
+    {
+        IDictionary<string, Session>? sessions = (ConcurrentDictionary<string, Session>?)state;
+        var sessionIds = (sessions ?? ImmutableDictionary<string, Session>.Empty).Keys.ToImmutableArray();
+        foreach (var sessionId in sessionIds)
+        {
+            UpdateSession(sessionId);
+        }
+    }
+
+    private void UpdateSession(string sessionId)
+    {
+        _logger.LogInformation("Updating session with ID: {SessionId}", sessionId);
+        var database = _redis.GetDatabase();
+        var value = database.StringGet(sessionId);
+        if (value.IsNull)
+            return;
+
+        var session = JsonSerializer.Deserialize<SessionEntity>((string)value);
+        if (session == null)
+            return;
+
+        var updatedSession = session with { UpdatedAt = DateTime.Now };
+        database.StringSet(sessionId, JsonSerializer.Serialize(updatedSession));
     }
 
     private static string ReadResource(string resourceName)
