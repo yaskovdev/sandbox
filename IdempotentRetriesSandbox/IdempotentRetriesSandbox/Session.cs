6ea8d34d62d32491bc6bae2110e92d92fc47f2b7
Sergey Yaskov <yaskovdev@gmail.com>
2025-05-31 20:57:16 +0300

    Add chaos engineering support to session management with Polly

--- IdempotentRetriesSandbox/IdempotentRetriesSandbox/Session.cs
+++ IdempotentRetriesSandbox/IdempotentRetriesSandbox/Session.cs
@@ -1,21 +1,37 @@
 namespace IdempotentRetriesSandbox;
 
 using System.Text.Json;
+using Polly.Contrib.Simmy;
+using Polly.Contrib.Simmy.Outcomes;
 using StackExchange.Redis;
 
 // ReSharper disable once ClassNeverInstantiated.Global, this is instantiated by the factory
 public class Session : IDisposable
 {
+    private readonly string _id;
     private readonly IConnectionMultiplexer _redis;
     private readonly ILogger<Session> _logger;
     private readonly Timer _timer;
+    private readonly InjectOutcomePolicy _chaosPolicy;
     private int _disposed;
 
     public Session(string id, IConnectionMultiplexer redis, ILogger<Session> logger)
     {
+        _id = id;
         _redis = redis;
         _logger = logger;
-        _timer = new Timer(UpdateSession, id, TimeSpan.Zero, TimeSpan.FromSeconds(5));
+        _timer = new Timer(UpdateSession, id, TimeSpan.Zero, TimeSpan.FromSeconds(3));
+        _chaosPolicy = MonkeyPolicy.InjectException(with =>
+            with.Fault(new InvalidOperationException("Cannot start session, Media Platform is down"))
+                .InjectionRate(0.5)
+                .Enabled(true)
+        );
+        Start();
+    }
+
+    private void Start()
+    {
+        _chaosPolicy.Execute(() => _logger.LogInformation("Session with ID: {SessionId} started", _id));
     }
 
     public void Dispose()
