ca68b09535723acd64ac3bc0bf47e3b6168c0811
Sergey Yaskov <yaskovdev@gmail.com>
2025-06-14 18:22:36 +0300

    Working draft.

--- service-instance-estimation/test_util.py
+++ service-instance-estimation/test_util.py
@@ -13,19 +13,19 @@ class TestUtil(TestCase):
         composition_end_time = calculate_composition_end_time(2, session_start_time, session_end_time, composition_start_time)
         self.assertEqual(datetime(2024, 1, 1, 9, 12), composition_end_time)
 
-    def test_composition_started_during_session_close_to_beginning(self):
-        session_start_time = datetime(2024, 1, 1, 9, 0)
-        session_end_time = datetime(2024, 1, 1, 9, 8)
-        composition_start_time = datetime(2024, 1, 1, 9, 2)
-        composition_end_time = calculate_composition_end_time(2, session_start_time, session_end_time, composition_start_time)
-        self.assertEqual(datetime(2024, 1, 1, 9, 8), composition_end_time)
-
     def test_composition_started_during_session_close_to_end(self):
         session_start_time = datetime(2024, 1, 1, 9, 0)
         session_end_time = datetime(2024, 1, 1, 9, 8)
         composition_start_time = datetime(2024, 1, 1, 9, 6)
         composition_end_time = calculate_composition_end_time(2, session_start_time, session_end_time, composition_start_time)
         self.assertEqual(datetime(2024, 1, 1, 9, 10), composition_end_time)
+
+    def test_composition_started_during_session_close_to_beginning(self):
+        session_start_time = datetime(2024, 1, 1, 9, 0)
+        session_end_time = datetime(2024, 1, 1, 9, 8)
+        composition_start_time = datetime(2024, 1, 1, 9, 2)
+        composition_end_time = calculate_composition_end_time(2, session_start_time, session_end_time, composition_start_time)
+        self.assertEqual(datetime(2024, 1, 1, 9, 8), composition_end_time)
 
     def test_composition_started_on_session_start(self):
         session_start_time = datetime(2024, 1, 1, 9, 0)
--- service-instance-estimation/util.py
+++ service-instance-estimation/util.py
@@ -9,17 +9,24 @@ def calculate_composition_end_time(faster_than_real_time_coefficient, session_st
         return composition_start_time + session_duration / faster_than_real_time_coefficient
 
     # composition started before session ended
-    closing_speed = faster_than_real_time_coefficient - 1  # real time speed is always 1
-    catchup_duration = min((composition_start_time - session_start_time) / closing_speed, session_end_time - composition_start_time)
+    producing_speed = 1
+    consuming_speed = producing_speed * faster_than_real_time_coefficient
+    closing_speed = consuming_speed - producing_speed  # real time speed is always 1
+
+    produced_at_composition_start_time = (composition_start_time - session_start_time) * producing_speed
 
     # at this moment either the session is ended, or it is ongoing, but we caught up
-    catchup_time = composition_start_time + catchup_duration
+    catchup_or_session_end_time = min(composition_start_time + produced_at_composition_start_time / closing_speed, session_end_time)
 
-    if session_end_time > catchup_time:
-        # we caught up before session ended
-        return session_end_time
+    if session_end_time == catchup_or_session_end_time:
+        composition_duration = catchup_or_session_end_time - composition_start_time
+        composed_amount = consuming_speed * composition_duration
+        total_amount = session_duration * producing_speed
+        remaining_amount = total_amount - composed_amount
+        return catchup_or_session_end_time + remaining_amount / consuming_speed
     else:
-        # session_end_time == catchup_time, i.e., session ended
-        duration_to_process_after_catchup = catchup_time - session_end_time
-        duration_to_process_after_catchup = session_duration - faster_than_real_time_coefficient * catchup_duration
-        return composition_start_time + catchup_duration + duration_to_process_after_catchup
+        composition_duration = catchup_or_session_end_time - composition_start_time
+        composed_amount = consuming_speed * composition_duration
+        total_amount = session_duration * producing_speed
+        remaining_amount = total_amount - composed_amount
+        return catchup_or_session_end_time + remaining_amount / producing_speed
