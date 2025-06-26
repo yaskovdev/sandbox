a001dbed52c9ea389c5a1ca3f9d9d6413f6cad6b
Sergey Yaskov <yaskovdev@gmail.com>
2025-06-26 18:43:24 +0300

    Create a simple message sender.

---
+++ AzureServiceBusSandbox/AzureServiceBusMessageSender/Program.cs
@@ -0,0 +1,21 @@
+﻿using Azure.Messaging.ServiceBus;
+
+namespace AzureServiceBusMessageSender;
+
+internal static class Program
+{
+    public static async Task Main(string[] args)
+    {
+        var connectionString = "<connection-string>";
+        var queueName = "session-composition-queue";
+        var client = new ServiceBusClient(connectionString);
+        var sender = client.CreateSender(queueName);
+
+        var uuid = Guid.NewGuid().ToString();
+        var message = new ServiceBusMessage(uuid);
+
+        await sender.SendMessageAsync(message);
+
+        Console.WriteLine($"Sent message with UUID: {uuid}");
+    }
+}
