2a3f416b9303f884e8b4f00fc124a6b401f0ebed
Sergey Yaskov <yaskovdev@gmail.com>
2026-01-12 10:50:56 +0200

    Cleanup the test.

--- AzureServiceBusExponentialBackoff/AzureServiceBusExponentialBackoff.Tests/AzureServiceBusExponentialBackoffIntegrationTests.cs
+++ AzureServiceBusExponentialBackoff/AzureServiceBusExponentialBackoff.Tests/AzureServiceBusExponentialBackoffIntegrationTests.cs
@@ -18,55 +18,50 @@ public class AzureServiceBusExponentialBackoffIntegrationTests
         var sender = client.CreateSender(QueueName);
         var receiver = client.CreateReceiver(QueueName);
 
-        // while (true)
-        // {
-        //     var m = await receiver.ReceiveMessageAsync();
-        //     await receiver.CompleteMessageAsync(m);
-        //     // var receiveDeferredMessageAsync = await receiver.ReceiveDeferredMessageAsync(194);
-        //     // await receiver.CompleteMessageAsync(receiveDeferredMessageAsync);
-        // }
-
         await sender.SendMessageAsync(new ServiceBusMessage("Hello World!"));
+
+        // Send reference message but fail to defer the original message
         var message = await receiver.ReceiveMessageAsync();
-
-        var reference = new ServiceBusMessage(JsonSerializer.Serialize(new MessageReference(message.SequenceNumber)))
-        {
-            MessageId = $"{message.MessageId}_ref_0",
-            ScheduledEnqueueTime = DateTimeOffset.UtcNow.AddSeconds(2)
-        };
-
+        var reference = CreateReferenceMessage(message, 0);
         await sender.SendMessageAsync(reference);
         await receiver.AbandonMessageAsync(message);
 
+        // Send reference message and defer the original message
         var originalMessageAgain = await receiver.ReceiveMessageAsync();
-        var anotherReference = new ServiceBusMessage(JsonSerializer.Serialize(new MessageReference(originalMessageAgain.SequenceNumber)))
-        {
-            MessageId = $"{message.MessageId}_ref_1",
-            ScheduledEnqueueTime = DateTimeOffset.UtcNow.AddSeconds(2)
-        };
+        var anotherReference = CreateReferenceMessage(originalMessageAgain, 1);
         await sender.SendMessageAsync(anotherReference);
         await receiver.DeferMessageAsync(originalMessageAgain);
 
         var counter = new Counter();
-        await Task.WhenAll(ProcessMessage(1, counter), ProcessMessage(2, counter));
+        await Task.WhenAll(ProcessMessage(0, counter), ProcessMessage(1, counter));
         counter.Value.ShouldBe(1, "The original message should be processed only once");
     }
 
-    private async Task ProcessMessage(int threadIndex, Counter counter)
+    private static ServiceBusMessage CreateReferenceMessage(ServiceBusReceivedMessage message, int refIndex) =>
+        new(JsonSerializer.Serialize(new MessageReference(message.SequenceNumber)))
+        {
+            MessageId = $"{message.MessageId}_ref_{refIndex}"
+        };
+
+    private static async Task ProcessMessage(int threadIndex, Counter counter)
     {
         var client = new ServiceBusClient(ServiceBusConnectionString);
         var receiver = client.CreateReceiver(QueueName);
+        var refMessage = await receiver.ReceiveMessageAsync();
         try
         {
-            var refMessage = await receiver.ReceiveMessageAsync();
             var body = JsonSerializer.Deserialize<MessageReference>(refMessage.Body);
             Console.WriteLine($"Thread {threadIndex} received ref message with body: {refMessage.Body}");
             var deferredMessage = await receiver.ReceiveDeferredMessageAsync(body.ReferencedMessageSequenceNumber);
             Console.WriteLine($"Thread {threadIndex} started processing deferred message");
             counter.Increment();
-            Thread.Sleep(5000);
             Console.WriteLine($"Thread {threadIndex} finished processing deferred message");
             await receiver.CompleteMessageAsync(deferredMessage);
+            await receiver.CompleteMessageAsync(refMessage);
+        }
+        catch (ServiceBusException e) when (e.Reason == ServiceBusFailureReason.MessageNotFound)
+        {
+            Console.WriteLine($"The original deferred message was not found: {e.Message}. Most likely it expired, got processed, or wasn't deferred. Completing the ref message to stop further retries");
             await receiver.CompleteMessageAsync(refMessage);
         }
         catch
